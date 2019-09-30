790058e784670ab22e47969f67df1a16ade46f1e
yuki.furuno <9837ghaz@jcom.zaq.ne.jp>
2019-09-30 14:02:07 +0000

    timeline

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,16 @@
+class FavoritesController < ApplicationController
+  def create
+    #@micropostとはならないよ
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.like(micropost)
+    flash[:success] = 'お気に入り登録をしました。'
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unlike(micropost)
+    flash[:success] = 'お気に入り登録を解除しました。'
+    redirect_back(fallback_location: root_path)
+  end
+end
